62db14115e76e7b8c8f3417797e05773beebff56
skeezr <skeeezr@gmail.com>
2023-04-12 10:20:39 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_dao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# csharp-dao
+ Exercice Console C# .NET 6.0, BDD DAO
